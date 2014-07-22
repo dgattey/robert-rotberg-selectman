816f81913c70211ba5d834afd63a5e1e4ae66c60
Dylan Gattey <dylan_gattey@brown.edu>
2014-07-21 22:23:47 -0700

    Removing even more Angular cruft, updating CDN paths to use latest... Oops.

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,8 @@
-'use strict';
-
-
-// Declare app level module which depends on filters, and services
 angular.module('rotbergApp', [
-  'timer',
+  // Built in
   'ngRoute',
-  'rotbergApp.filters',
-  'rotbergApp.services',
-  'rotbergApp.directives',
-  'rotbergApp.controllers',
-  'ui.bootstrap'
+  'ui.bootstrap',
+
+  // Extras
+  'timer'
 ]);
