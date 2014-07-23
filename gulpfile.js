1762257c08c71e447fe698b036ba8844593ad901
Dylan Gattey <dylan_gattey@brown.edu>
2014-07-23 02:29:31 -0700

    1) Webserver included, 2) gulp fat included, does no minification, 3) Fixed all errors so back to how it was prior to gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,110 @@
-// Plugins
 var gulp        = require('gulp'),
     jshint      = require('gulp-jshint'),
-    stylish     = require('jshint-stylish'),
     sass        = require('gulp-sass'),
     concat      = require('gulp-concat'),
     uglify      = require('gulp-uglify'),
     minifyCSS   = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
-    rimraf      = require('rimraf');
+    webserver   = require('gulp-webserver'),
+    stylish     = require('jshint-stylish'),
+    rimraf      = require('rimraf'),
+    cfg         = require('./gulp.config.js');
 
-// Configuration
-var cfg = require( './gulp.config.js' );
+/**
+ * SASS
+ * Combines and minifies all SASS files
+ */
+var sassFuncFat = function() {
+    return gulp.src(cfg.srcFiles.sass)
+        .pipe(sass())
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest(cfg.dest));
+};
+var sassFunc = function() {
+    return sassFuncFat()
+        .pipe(minifyCSS({processImport:true}))
+        .pipe(gulp.dest(cfg.dest));
+};
+gulp.task('sass',  sassFunc);
+gulp.task('sass-fat',  sassFuncFat);
+gulp.task('sass-clean', ['clean'], sassFunc);
+gulp.task('sass-clean-fat', ['clean'], sassFuncFat);
 
-// Cleaning Task
-gulp.task('clean', function(cb) {
-    return rimraf(cfg.dest, cb);
-});
+/**
+ * STATIC FILES
+ * Copies static files to the build directory
+ */
+var htmlFunc = function(){
+    return gulp.src(cfg.srcFiles.html)
+        .pipe(gulp.dest(cfg.dest));
+};
+var assetsFunc = function(){
+    return gulp.src(cfg.srcFiles.assets)
+        .pipe(gulp.dest(cfg.dest+'/assets/'));
+};
+gulp.task('html', htmlFunc);
+gulp.task('html-clean', ['clean'], htmlFunc);
+gulp.task('assets', assetsFunc);
+gulp.task('assets-clean', ['clean'], assetsFunc);
 
-// Lint Tasks
+/**
+ * JAVASCRIPT
+ * Concatenate and minify JS files after linting
+ */
 var lintFunc = function() {
     return gulp.src(cfg.srcFiles.js)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 };
-gulp.task('lint', lintFunc);
-gulp.task('lint-clean', ['clean'], lintFunc);
-
-// SASS to CSS and minify
-var sassFunc = function() {
-    return gulp.src(cfg.srcFiles.sass)
-        .pipe(sass())
-        .pipe(minifyCSS({processImport:true}))
-        .pipe(rename('all.min.css'))
-        .pipe(gulp.dest(cfg.dest));
-};
-gulp.task('sass',  sassFunc);
-gulp.task('sass-clean', ['clean'], sassFunc);
-
-// Copy HTML files
-var htmlFunc = function(){
-    return gulp.src(cfg.srcFiles.html)
-        .pipe(gulp.dest(cfg.dest));
-};
-gulp.task('html', htmlFunc);
-gulp.task('html-clean', ['clean'], htmlFunc);
-
-// Copy Assets
-var assetsFunc = function(){
-    return gulp.src(cfg.srcFiles.assets)
-        .pipe(gulp.dest(cfg.dest));
-};
-gulp.task('assets', assetsFunc);
-gulp.task('assets-clean', ['clean'], assetsFunc);
-
-// Concatenate & Minify JS
-var jsFunc = function() {
+var jsFuncFat = function() {
     return gulp.src(cfg.srcFiles.allJS)
         .pipe(concat('all.min.js'))
-        .pipe(gulp.dest(cfg.dest))
-        .pipe(uglify())
         .pipe(gulp.dest(cfg.dest));
 };
+var jsFunc = function() {
+    return jsFuncFat()
+        .pipe(uglify())
+        .pipe(gulp.dest(cfg.dest));
+}
+gulp.task('lint', lintFunc);
+gulp.task('lint-clean', ['clean'], lintFunc);
 gulp.task('js', ['lint'],  jsFunc);
+gulp.task('js-fat', jsFuncFat);
 gulp.task('js-clean', ['lint-clean'],  jsFunc);
+gulp.task('js-clean-fat', ['lint-clean'],  jsFuncFat);
 
-// Watching all files
+/**
+ * ONGOING
+ * Tasks that are ongoing, like serving files and
+ * watching for changes
+ */
 gulp.task('watch', ['dist'], function() {
     gulp.watch(cfg.srcFiles.allJS, ['lint', 'js']);
     gulp.watch(cfg.srcFiles.sass, ['sass']);
     gulp.watch(cfg.srcFiles.assets, ['assets']);
     gulp.watch(cfg.srcFiles.html, ['html']);
 });
+gulp.task('watch-fat', ['dev'], function() {
+    gulp.watch(cfg.srcFiles.allJS, ['lint', 'js-fat']);
+    gulp.watch(cfg.srcFiles.sass, ['sass-fat']);
+    gulp.watch(cfg.srcFiles.assets, ['assets']);
+    gulp.watch(cfg.srcFiles.html, ['html']);
+});
+gulp.task('server', function() {
+    gulp.src(cfg.dest)
+        .pipe(webserver({
+            livereload: true
+        }));
+});
 
-// Default Tasks
-gulp.task('dist', ['lint-clean', 'sass-clean', 'assets-clean', 'html-clean', 'js-clean'])
-gulp.task('default', ['clean', 'dist', 'watch']);
+/**
+ * MAIN
+ * Overarching tasks for full build
+ */
+gulp.task('clean', function(cb) {
+    return rimraf(cfg.dest, cb);
+});
+gulp.task('dev', ['lint-clean', 'sass-clean-fat', 'assets-clean', 'html-clean', 'js-clean-fat']);
+gulp.task('dist', ['lint-clean', 'sass-clean', 'assets-clean', 'html-clean', 'js-clean']);
+gulp.task('fat', ['clean', 'dev', 'watch-fat', 'server']);
+gulp.task('default', ['clean', 'dist', 'watch', 'server']);
--- src/app/app.js
+++ src/app/app.js
@@ -1,35 +1,40 @@
+var navigation = [
+{'title':'Donate','icon':'flash','url':'donate'},
+/*{'title':'Endorse','icon':'certificate','url':'endorse'},*/
+{'title':'Send Message','icon':'comment','url':'contact'}
+];
+
 /**
  * APP
  * The module that runs the full application
  */
-var navigation;
-angular.module('rotbergApp', [
+ angular.module('rotbergApp', [
   'ui.bootstrap',
   'ui.router',
   'app.views',
   'app.vendor'
-]).config(function ($stateProvider, $urlRouterProvider) {
+  ])
 
-  // Setting up Parse
-  if (Parse){
-     Parse.initialize('ho6HnNe3rRvhLvSVdZqizGy2BIXiPdCov4AIrKBL', '89LDCNDNImDFnKDZBL8C9mr0HB641SXMzeLExIru');
-  }
+  // Sets up routing and Parse
+  .config(['$stateProvider', function ($stateProvider) {
 
-  $urlRouterProvider.otherwise('/');
-  navigation = [
-    {'title':'Donate','icon':'flash','url':'donate'},
-    /*{'title':'Endorse','icon':'certificate','url':'endorse'},*/
-    {'title':'Send Message','icon':'comment','url':'contact'}
-  ];
-  for (var i = 0; i < navigation.length; i++) {
-    var view = navigation[i];
-    $stateProvider
-      .state(view.url, {
+    if (Parse){
+      Parse.initialize(
+        'ho6HnNe3rRvhLvSVdZqizGy2BIXiPdCov4AIrKBL', // API
+        '89LDCNDNImDFnKDZBL8C9mr0HB641SXMzeLExIru' // This app
+      );
+    }
+
+    // Create routes
+    for (var i = 0; i < navigation.length; i++) {
+      var view = navigation[i];
+      $stateProvider.state(view.url, {
         url: '/'+view.url,
         templateUrl: 'app/views/'+view.url+'/'+view.url+'.tpl.html'
       });
-  }
+    }
+  }])
 
-}).controller('ButtonCtrl', function ($scope) {
-  $scope.buttons = navigation;
-});
+ .controller('ButtonCtrl', ['$scope', function ($scope) {
+    $scope.buttons = navigation;
+  }]);
