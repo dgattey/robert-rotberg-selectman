acc0f30b032e6d425e580e2d786a80bf02cd690e
Dylan Gattey <dylan_gattey@brown.edu>
2014-07-23 03:36:32 -0700

    Shows message for user and double error checking

--- src/app/views/contact/contact.js
+++ src/app/views/contact/contact.js
@@ -1,1 +1,19 @@
-angular.module('app.views.contact', []);
+angular.module('app.views.contact', [])
+  .controller('ContactCtrl', ['$scope', function ($scope) {
+
+    $scope.submitMessage = function(form) {
+      if (form.$invalid) {
+        $scope.showGenericError = true;
+        return;
+      }
+
+      //SEND MESSAGE HERE VIA NODEMAILER!
+
+      $scope.firstName = $scope.name.substr(0, $scope.name.indexOf(' '));
+      if ($scope.firstName === '') {
+        $scope.firstName = $scope.name;
+      }
+      $scope.showConfirmation = true;
+    };
+
+  }]);
