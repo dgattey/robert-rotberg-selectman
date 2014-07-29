2d1cfe95d5894127707daf03b880a76fe880c17a
Dylan Gattey <dylan_gattey@brown.edu>
2014-07-29 02:53:26 -0700

    Getting rid of Parse and moving http config to app.js

--- src/app/app.js
+++ src/app/app.js
@@ -16,14 +16,7 @@ var navigation = [
   ])
 
   // Sets up routing and Parse
-  .config(['$stateProvider', function ($stateProvider) {
-
-    if (Parse){
-      Parse.initialize(
-        'ho6HnNe3rRvhLvSVdZqizGy2BIXiPdCov4AIrKBL', // API
-        '89LDCNDNImDFnKDZBL8C9mr0HB641SXMzeLExIru' // This app
-      );
-    }
+  .config(['$stateProvider', '$httpProvider', function ($stateProvider, $httpProvider) {
 
     // Create routes
     for (var i = 0; i < navigation.length; i++) {
@@ -33,6 +26,47 @@ var navigation = [
         templateUrl: 'app/views/'+view.url+'/'+view.url+'.tpl.html'
       });
     }
+
+    // Use x-www-form-urlencoded Content-Type
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+    // Converts an object to x-www-form-urlencoded serialization.
+    var param = function(obj) {
+      var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+      for(name in obj) {
+        value = obj[name];
+
+        if(value instanceof Array) {
+          for(i=0; i<value.length; ++i) {
+            subValue = value[i];
+            fullSubName = name + '[' + i + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value instanceof Object) {
+          for(subName in value) {
+            subValue = value[subName];
+            fullSubName = name + '[' + subName + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value !== undefined && value !== null) {
+          query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }
+      }
+
+      return query.length ? query.substr(0, query.length - 1) : query;
+    };
+
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function(data) {
+      return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
   }])
 
  .controller('ButtonCtrl', ['$scope', function ($scope) {
--- src/app/views/contact/contact.js
+++ src/app/views/contact/contact.js
@@ -1,48 +1,5 @@
 angular.module('app.views.contact', [])
   .controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
-    // Use x-www-form-urlencoded Content-Type
-    $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-
-    /**
-     * The workhorse; converts an object to x-www-form-urlencoded serialization.
-     */
-    var param = function(obj) {
-      var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
-
-      for(name in obj) {
-        value = obj[name];
-
-        if(value instanceof Array) {
-          for(i=0; i<value.length; ++i) {
-            subValue = value[i];
-            fullSubName = name + '[' + i + ']';
-            innerObj = {};
-            innerObj[fullSubName] = subValue;
-            query += param(innerObj) + '&';
-          }
-        }
-        else if(value instanceof Object) {
-          for(subName in value) {
-            subValue = value[subName];
-            fullSubName = name + '[' + subName + ']';
-            innerObj = {};
-            innerObj[fullSubName] = subValue;
-            query += param(innerObj) + '&';
-          }
-        }
-        else if(value !== undefined && value !== null) {
-          query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-        }
-      }
-
-      return query.length ? query.substr(0, query.length - 1) : query;
-    };
-
-    // Override $http service's default transformRequest
-    $http.defaults.transformRequest = [function(data) {
-      return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-    }];
-
     $scope.submitMessage = 'Send Message';
 
     $scope.clearError = function() {
