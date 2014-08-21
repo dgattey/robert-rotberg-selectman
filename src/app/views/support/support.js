062f486644632ee62cdc7f144f109eb54747cd9f
Dylan Gattey <dylan_gattey@brown.edu>
2014-08-20 17:38:22 -0700

    Clearer breakdown of data in support, and support for displayName + public employee

--- src/app/views/support/support.js
+++ src/app/views/support/support.js
@@ -1,6 +1,16 @@
 angular.module('app.views.support', [])
   .controller('SupportCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.submitMessage = 'Submit Endorsement';
+    $scope.formData = {
+      name: '',
+      displayName: 'wholeName',
+      publicEmployee: 'no',
+      message: ''
+    };
+
+    var getFirstName = function(name) {
+      return name && name !== '' ? name.split(' ')[0] : name;
+    };
 
     $scope.clearError = function() {
       $scope.failCount = 0;
@@ -13,23 +23,38 @@ angular.module('app.views.support', [])
         return;
       }
 
+      //Compose Data
+      var postObj = {
+        name: $scope.formData.name || ' ',
+        displayName: 'Anonymous',
+        endorsement: $scope.formData.message || ' '
+      };
+      if ($scope.formData.publicEmployee === 'no') {
+        switch ($scope.formData.displayName) {
+          case 'firstName':
+            postObj.displayName = getFirstName(postObj.name) || ' ';
+            break;
+          case 'wholeName':
+            postObj.displayName = postObj.name;
+            break;
+          default:
+            break;
+        }
+      } else {
+        postObj.displayName = 'Public Employee';
+      }
+
       //Send email message
       $scope.submitDisabled = true;
       $scope.submitMessage = 'Endorsing ...';
-      $http.post('php/sendMail.php', {
-        'name' : $scope.name || ' ',
-        'endorsement' : $scope.message || ' '
-      }).
+      $http.post('php/sendMail.php', postObj).
       success(function() {
-        $scope.firstName = $scope.name.substr(0, $scope.name.indexOf(' '));
-        if ($scope.firstName === '') {
-          $scope.firstName = $scope.name;
-        }
+        $scope.formData.firstName = getFirstName($scope.formData.name);
         $scope.showConfirmation = true;
       }).
       error(function(data, status) {
         $scope.failCount = $scope.failCount + 1 || 1;
-        if ($scope.failCount >= 2) {
+        if ($scope.failCount >= 4) {
           $scope.formError = 'Seems like there are some problems. Email us at info@robertrotberg.com if they persist.';
         } else {
           $scope.formError = 'Emailing form data failed with status '+status+' (' + data +')';
