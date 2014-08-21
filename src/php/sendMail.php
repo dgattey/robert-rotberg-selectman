42d3f5ce7d41e125f39f7d4edb3be65a643d2dd4
Dylan Gattey <dylan_gattey@brown.edu>
2014-08-20 18:32:38 -0700

    Updated PHP for new endorse form

--- src/php/sendMail.php
+++ src/php/sendMail.php
@@ -14,19 +14,17 @@ function clean_string($string) {
   return str_replace($bad,"",$string);
 }
 
-// Get post data
-$name = $_POST['name']; // required
-$email_from = $_POST['email']; // required
-$phone = $_POST['phone']; // not required
-$message = $_POST['message']; // required, or below
-$endorse = $_POST['endorsement']; // required, or above
+// Shared data
+$name = $_POST['name'];
+$message = $_POST['message'];
+// Endorse only
+$display_name $_POST['displayName'];
+// Contact only
+$email_from = $_POST['email'];
+$phone = $_POST['phone'];
 
 // Validation
-if (!isset($endorse) && !isset($message)) {
-    died('One of the required fields was missing');
-} else if (isset($message) &&  (!isset($name) || !isset($email_from) || !isset($message) || !isset($phone))) {
-  died('One of the required fields was missing');
-} else if (isset($endorse) && !isset($name)) {
+if (!isset($message) || !isset($name)) {
   died('One of the required fields was missing');
 }
 $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -37,8 +35,8 @@ $string_exp = "/^[A-Za-z .'-]+$/";
 if(!preg_match($string_exp,$name)) {
   died('Your name does not appear to be valid');
 }
-if(!isset($endorse) && isset($message) && strlen($message) < 2) {
-  died('Your message is invalid');
+if(isset($display_name) && !preg_match($string_exp,$display_name)) {
+  died('Your display name does not appear to be valid');
 }
 
 // Clean inputs
@@ -52,11 +50,9 @@ if (isset($phone)) {
 }
 if (isset($message)){
   $message = clean_string($message);
-} else if (isset($endorse)) {
-  $endorse = clean_string($endorse);
 }
 
-if (isset($message)) {
+if (!isset($display_name)) {
   // HTML body
   $body = file_get_contents('../app/views/email-template/basic.html');
   $body = str_replace('$name', $name, $body);
@@ -71,11 +67,12 @@ if (isset($message)) {
   $body_plain .= "Email: ".$email_from."\n";
   $body_plain .= "Phone: ".$phone."\n";
   $body_plain .= "Comments: ".$message."\n";
-} else if (isset($endorse)) {
+} else {
   // Plaintext body
-  $body = "Hey there! You have a new endorsement through http://robertrotberg.com.\n\n\n";
-  $body .= "From: ".$name."\n";
-  $body .= "Message: ".$endorse."\n";
+  $body = "Hey there! You have a new endorsement through http://robertrotberg.com.\n\n";
+  $body .= "Full Name: ".$name."\n";
+  $body .= "Name should be displayed on website as: ".$display_name."\n";
+  $body .= "Optional comments: ".$message."\n";
 
   $body_plain = $body;
 }
@@ -95,7 +92,7 @@ $mailer->Username = 'skicrazy27@gmail.com';             // SMTP username
 $mailer->Password = 'ajxhjhyjibmtxxpf';                 // SMTP password
 $mailer->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
 $mailer->WordWrap = 50;                                 // Set word wrap to 50 characters
-$mailer->isHTML(isset($message));                                  // Set email format to HTML
+$mailer->isHTML(true);                                  // Set email format to HTML
 
 // Configuration of addresses
 if (!isset($email_from)) {
