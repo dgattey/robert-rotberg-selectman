dddfa7be19cc7a3e54488980f1c06db0db5de0ec
Dylan Gattey <dylan_gattey@brown.edu>
2014-08-26 11:54:02 -0400

    Names added, and endorsements now load up to 1000 entries. Also ordered by last name, then first name

--- src/app/views/endorsements/endorsements.js
+++ src/app/views/endorsements/endorsements.js
@@ -2,21 +2,31 @@ angular.module('app.views.endorse', [])
   .controller('EndorseCtrl', ['$scope', function ($scope) {
     $scope.endorsements = [];
     $scope.errorLoadingEndorsements = false;
-    var EndorsementCollection = Parse.Collection.extend({
-      model: Parse.Object.extend('Endorsement')
-    });
 
-    var collection = new EndorsementCollection();
-    collection.fetch({
+    var Endorsement = Parse.Object.extend('Endorsement');
+    var query = new Parse.Query(Endorsement);
+    query.limit(1000);
+    query.find({
       success: function(collection) {
+
         $scope.$apply(function(){
-          collection.each(function(endorsement) {
-            $scope.endorsements.push({name:endorsement.get('name')});
-          });
+          for (var i = 0; i < collection.length; i++) {
+            var name = collection[i].get('name'),
+                withoutEnding = name.split(',')[0].trim(),
+                nameArray = withoutEnding.split(' '),
+                lastName = nameArray[nameArray.length-1].trim();
+            if (!lastName || lastName === '' || lastName === ' '){
+              lastName = nameArray[nameArray.length-2].trim();
+            }
+            $scope.endorsements.push({
+              name: name,
+              lastName: lastName
+            });
+          }
         });
       },
       error: function(collection, error) {
-        console.warn('Could not retrieve endorsements because ' + error);
+        console.warn('Could not retrieve endorsements because ', error);
         $scope.errorLoadingEndorsements = true;
       }
     });
